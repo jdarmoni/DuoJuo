2cdb66eab3448dd40845a588ae8d0e6ac0fcc61e
Jdarmoni <jgdarmoni@gmail.com>
2019-05-28 17:25:35 -0400

    user controller

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,4 @@
+class Api::UsersController < ApplicationController
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    namespace :api, defaults: {format: :json} do
+        resource :user, only: [:create]
+        resource :session, only: [:create, :destroy] 
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "static_pages#root"
 
