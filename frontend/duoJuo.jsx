409ef36bbe7deb743740b24d22038c033d237c68
Jdarmoni <jgdarmoni@gmail.com>
2019-05-28 17:53:22 -0400

    frontend and routes

--- frontend/duoJuo.jsx
+++ frontend/duoJuo.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to DuoJuo</h1>, root);
+    ReactDOM.render(<h1>eggs</h1>, root);
 });
---
+++ frontend/store/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+// import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const configureStore = (preloadedState = {}) => {
+    return createStore(
+        rootReducer, preloadedState, applyMiddleware(thunk)
+    )
+}
+
+export default configureStore;
