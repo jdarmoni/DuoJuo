f8db33b98b589ca6d5f56688ece0d4af3b838317
Jdarmoni <jgdarmoni@gmail.com>
2019-05-28 17:14:54 -0400

    root html renders

---
+++ app/frontend/duoJuo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>Welcome to DuoJuo</h1>, root);
+});
