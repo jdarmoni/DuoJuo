16eb9416bd46ea43d4537345a43ca9ba8549c791
Jdarmoni <jgdarmoni@gmail.com>
2019-06-12 17:24:24 -0400

    configure user routes appropriately (plural resources instead of singular)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
     namespace :api, defaults: {format: :json} do
-        resource :user, only: [:show, :create]
+        resources :user, only: [:show, :create, :update]
         resource :session, only: [:create, :destroy] 
-        resource :languages, only: [:index, :show, :create, :update]
+        resources :languages, only: [:index, :show, :create, :update]
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "static_pages#root"
