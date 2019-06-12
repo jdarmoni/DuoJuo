fdb96593f5aefdae6b0e25de474131c33e35844d
Jdarmoni <jgdarmoni@gmail.com>
2019-06-12 17:07:34 -0400

    user actions

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,18 @@
+import {updateUser} from '../util/user_api_util'
+
+export const RECEIVE_USER = "RECEIVE_USER"
+
+export const receiveUser = (user)=>{
+    return ({
+        type: RECEIVE_USER,
+        user: user
+    })
+}
+
+export const updateUser =(user)=> (dispatch)=>{
+    return (
+        updateUser(user).then((user)=>{
+            return dispatch(receiveUser(user))
+        })
+    )
+}
