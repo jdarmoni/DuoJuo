46d57c4f6e87a3171f059a10bde3cca3ed365b33
Jdarmoni <jgdarmoni@gmail.com>
2019-06-30 18:35:32 -0400

    user Object.Assign instead of merge to return state with language data key

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -4551,10 +4551,11 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
     case _actions_session_actions__WEBPACK_IMPORTED_MODULE_0__["LOGOUT_CURRENT_USER"]:
       // i added this so that you don't accumulate users throughout login/logouts
       return {};
-    // case RECEIVE_LANGUAGE_DATA:
-    //     // comment this out to repair
-    //     debugger
-    //     return merge({}, state[action.language_data.user_id], { ["language_data"]: {[action.language_data.language]:  action.language_data }});
+
+    case _actions_language_data_actions__WEBPACK_IMPORTED_MODULE_1__["RECEIVE_LANGUAGE_DATA"]:
+      // comment this out to repair
+      debugger;
+      return Object.assign({}, state[action.language_data.user_id], _defineProperty({}, "language_data", _defineProperty({}, action.language_data.language, action.language_data)));
     // case RECEIVE_SKILL:
     //     debugger
     //     return merge({}, Object.values(state[0]).language_data, {["skills"]: action.skill} )
@@ -4562,7 +4563,24 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
     default:
       return state;
   }
-});
+}); // return merge({}, Object.values(state)[0], { ["language_data"]: { [action.language_data.language]: action.language_data } });
+// Object.values(state.entities.users)
+//     (11)[85, "naLqRMm4", "naLqRMm4@gmail.com", null, null, false, null, "fr", "French", Array(0), { … }]
+// 0: 85
+// 1: "naLqRMm4"
+// 2: "naLqRMm4@gmail.com"
+// 3: null
+// 4: null
+// 5: false
+// 6: null
+// 7: "fr"
+// 8: "French"
+// 9: []
+// 10:
+// fr: { streak: 0, user_id: 85, language_string: "French", points: 0, learning: true }
+// __proto__: Object
+// length: 11
+// __proto__: Array(0)
 
 /***/ }),
 
--- frontend/reducers/user_reducer.js
+++ frontend/reducers/user_reducer.js
@@ -14,10 +14,10 @@ export default (state = {}, action) => {
         case LOGOUT_CURRENT_USER:
             // i added this so that you don't accumulate users throughout login/logouts
             return {}
-        // case RECEIVE_LANGUAGE_DATA:
-        //     // comment this out to repair
-        //     debugger
-        //     return merge({}, state[action.language_data.user_id], { ["language_data"]: {[action.language_data.language]:  action.language_data }});
+        case RECEIVE_LANGUAGE_DATA:
+            // comment this out to repair
+            debugger
+            return Object.assign({}, state[action.language_data.user_id], { ["language_data"]: {[action.language_data.language]:  action.language_data }});
         // case RECEIVE_SKILL:
         //     debugger
         //     return merge({}, Object.values(state[0]).language_data, {["skills"]: action.skill} )
@@ -25,3 +25,24 @@ export default (state = {}, action) => {
             return state;
     }
 }
+
+// return merge({}, Object.values(state)[0], { ["language_data"]: { [action.language_data.language]: action.language_data } });
+
+
+// Object.values(state.entities.users)
+//     (11)[85, "naLqRMm4", "naLqRMm4@gmail.com", null, null, false, null, "fr", "French", Array(0), { … }]
+// 0: 85
+// 1: "naLqRMm4"
+// 2: "naLqRMm4@gmail.com"
+// 3: null
+// 4: null
+// 5: false
+// 6: null
+// 7: "fr"
+// 8: "French"
+// 9: []
+// 10:
+// fr: { streak: 0, user_id: 85, language_string: "French", points: 0, learning: true }
+// __proto__: Object
+// length: 11
+// __proto__: Array(0)
