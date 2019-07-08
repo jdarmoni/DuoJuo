e5fc0cbd4866e2ac95c3553e7c7f0345b570cd6e
Jdarmoni <jgdarmoni@gmail.com>
2019-07-08 13:55:19 -0400

    initial commit for completed lesson

---
+++ frontend/components/lessons/lesson_complete/completed.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Completed extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+
+    }
+}
+
+export default (Completed)
---
+++ frontend/components/lessons/lesson_complete/completed_container.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import Completed from './completed'
+
+const mapStateToProps = (state) => {
+
+    return {
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Completed)
