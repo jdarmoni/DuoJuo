e72fa26b916f1b0ce313d87ead9fb37f45fbeeac
Jdarmoni <jgdarmoni@gmail.com>
2019-07-10 15:50:45 -0400

    change same migration to change :array (doesnt exit and throws error) to integer

--- db/migrate/20190602182552_add_to_user_table.rb
+++ db/migrate/20190602182552_add_to_user_table.rb
@@ -3,7 +3,7 @@ class AddToUserTable < ActiveRecord::Migration[5.2]
     add_column :users, :language_data, :integer
     add_column :users, :language_strength, :integer
     add_column :users, :site_streak, :integer
-    add_column :users, :languages, :array
+    add_column :users, :languages, :integer
     add_column :users, :learning_language, :string
     add_column :users, :learning_language_string, :string
     add_column :users, :rupees, :integer
