67abb2ba010203ca8625913a303f74b644ebec82
Jdarmoni <jgdarmoni@gmail.com>
2019-07-10 15:45:31 -0400

    edit migration (eep) in add_to_user_table to get rid of :object (doesn't exit and throws an error) into :integer

--- db/migrate/20190602182552_add_to_user_table.rb
+++ db/migrate/20190602182552_add_to_user_table.rb
@@ -1,6 +1,6 @@
 class AddToUserTable < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :language_data, :object
+    add_column :users, :language_data, :integer
     add_column :users, :language_strength, :integer
     add_column :users, :site_streak, :integer
     add_column :users, :languages, :array
