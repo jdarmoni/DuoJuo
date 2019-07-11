1f5b13ef70e0e76855201f1e5007e069ccb1c90c
Jdarmoni <jgdarmoni@gmail.com>
2019-07-10 20:31:11 -0400

    each language has three components

--- frontend/components/lessons/grand_lessons_obj/grand_lessons_obj.jsx
+++ frontend/components/lessons/grand_lessons_obj/grand_lessons_obj.jsx
@@ -9,24 +9,26 @@ import CompletedContainer from '../../lessons/lesson_complete/completed_containe
     // }
  export const grandLessonsObj = {
         "fr": {
-            "Basics-1": [<TranslateSentenceContainer sentence={"Le chien est epouvantable"} correct={["the dog is scary"]}/>, <TranslateSentenceContainer sentence={"Je suis un fil, pas un fille"} correct={["I am a boy, not a girl"]} />, <TranslateSentenceContainer sentence={"La fille, la femme"} correct={["the girl, the woman"]}/>, <MarkMeaningContainer sentence={["A French woman"]} correct={"Un femme Francaise"} phrase1={"Un femme Francaise"} phrase2={"Il est un pamplemousse"} phrase3={"Je suis le jeunne fille"}/>],
-            "Greetings": [<TranslateSentenceContainer sentence={"Bonjour, je cache des oeufs"} correct={["Hello, I am hiding eggs"]} />, <TranslateSentenceContainer sentence={"Tu va me dire ou est le lait maitenant"} correct={["You will tell me where the milk is now"]}/>],
-            "Basics-2": []
+         "Basics-1": [<TranslateSentenceContainer sentence={"Le chien est effrayant"} correct={["the dog is scary"]}/>, <TranslateSentenceContainer sentence={"Je suis un fil, pas un fille"} correct={["I am a boy, not a girl"]} />, <TranslateSentenceContainer sentence={"La fille, la femme"} correct={["the girl, the woman"]}/>, <MarkMeaningContainer sentence={["A French woman"]} correct={"Un femme Francaise"} phrase1={"Un femme Francaise"} phrase2={"Il est un pamplemousse"} phrase3={"Je suis le jeunne fille"}/>],
+         "Greetings": [<TranslateSentenceContainer sentence={"Bonjour, je cache des oeufs"} correct={["Hello, I am hiding eggs"]} />, <TranslateSentenceContainer sentence={"Capitaine Haddock boit à nouveau"} correct={["Captain Haddock is drinking again"]} />, <TranslateSentenceContainer sentence={"Tu va me dire ou est le lait maitenant"} correct={["You will tell me where the milk is now"]} />, <TranslateSentenceContainer sentence={"Juliette Binoche"} correct={["Juliette Binoche"]} />],
+         "Basics-2": [<TranslateSentenceContainer sentence={"Petite cerises"} correct={["little cherries"]} />, <TranslateSentenceContainer sentence={"Je suis seule a Dunkin Donuts"} correct={["I am alone at Dunkin Donuts"]} />, <TranslateSentenceContainer sentence={"D'ou etes-vous?"} correct={["Where are you from?"]} />, <TranslateSentenceContainer sentence={"Je suis un célébrité"} correct={["I am a celebrity"]} />]
         },
         "jp": {
-            "Basics-1": [<TranslateSentenceContainer sentence={"あなたは私の中央ビジネス地区への道を示すことができました"} correct={["Could you show me the way to the central business district"]} />, <TranslateSentenceContainer sentence={"私はインサイダー取引で非難されています"} correct={["I have been accused of insider trading"]} /> ],
-            "Intro": []
+            // 
+            "Basics-1": [<TranslateSentenceContainer sentence={"こんにちは、さようなら"} correct={["hello, goodbye"]} />, <TranslateSentenceContainer sentence={"私の名前は"} correct={["My name is"]} />, <TranslateSentenceContainer sentence={"水"} correct={["water"]} />, <TranslateSentenceContainer sentence={"たべる"} correct={["To eat"]} />],
+            "Intro": [<TranslateSentenceContainer sentence={"私の名前はマリアです"} correct={["My name is maria"]} />, <TranslateSentenceContainer sentence={"あなたは私の中央ビジネス地区への道を示すことができました"} correct={["Could you show me the way to the central business district"]} />, <TranslateSentenceContainer sentence={"アメリカ"} correct={["America"]} />, <TranslateSentenceContainer sentence={"私はインサイダー取引で非難されています"} correct={["I have been accused of insider trading"]} />  ],
+            "Basics-2": [<TranslateSentenceContainer sentence={"ポケットモンスター"} correct={["Pokemon"]} />, <TranslateSentenceContainer sentence={"男の子"} correct={["boy"]} />, <TranslateSentenceContainer sentence={"お父さん"} correct={["father"]} />, <TranslateSentenceContainer sentence={"母"} correct={["mother"]} />],
         },
         "ge": {
-            "Basics-1": [],
-            "Greetings": [],
-            "Basics-2": []
+            "Basics-1": [<TranslateSentenceContainer sentence={"Der junge"} correct={["The boy"]} />, <TranslateSentenceContainer sentence={"Das mädchen"} correct={["the girl"]} />, <TranslateSentenceContainer sentence={"Ich bin ein Berliner"} correct={["I am a Berliner"]} />, <TranslateSentenceContainer sentence={"Es tut mir leid"} correct={["I am sorry"]} />],
+            "Greetings": [<TranslateSentenceContainer sentence={"Hallo, ich bin der junge"} correct={["Hello, I am the boy"]} />, <TranslateSentenceContainer sentence={"Der film"} correct={["the film"]} />, <TranslateSentenceContainer sentence={"Ich heiße Christoph Waltz"} correct={["My name is Christoph Waltz"]} />, <TranslateSentenceContainer sentence={"Ich mag Duolingo"} correct={["I like Duolingo"]} />],
+            "Basics-2": [<TranslateSentenceContainer sentence={"Bitte"} correct={["Please"]} />, <TranslateSentenceContainer sentence={"Danke dir"} correct={["thank you"]} />, <TranslateSentenceContainer sentence={"the boy"} correct={["der junge"]} />, <TranslateSentenceContainer sentence={"I like Christoph Waltz"} correct={["Ich mag Christoph Waltz"]} />]
 
         },
         "es": {
             "Basics-1": [<TranslateSentenceContainer sentence={"el cerdo esta durmiendo"} correct={["the pig is sleeping"]} />, <TranslateSentenceContainer sentence={"Soy el chico que bebe leche"} correct={["I am the boy who drinks milk"]} />, <TranslateSentenceContainer sentence={"eres un pájaro?"} correct={["are you a bird?"]} />, <TranslateSentenceContainer sentence={"dime porque sueño"} correct={["tell me why i dream"]} />],
-            "Greetings": [],
-            "Basics-2": []
+            "Greetings": [<TranslateSentenceContainer sentence={"Mi nombre es rafael nadal"} correct={["My name is Rafael Nadal"]} />, <TranslateSentenceContainer sentence={"Soy apasionado"} correct={["I am passionate"]} />, <TranslateSentenceContainer sentence={"Mi mejor amigo se llama Roger"} correct={["My best friend is named Roger"]} />, <TranslateSentenceContainer sentence={"I have won Roland Garros 12 times, no?"} correct={["He ganado Roland Garros 12 veces, ¿no?"]} />],
+            "Basics-2": [<TranslateSentenceContainer sentence={"Yo soy de Mallorca"} correct={["I am from Mallorca"]} />, <TranslateSentenceContainer sentence={"Mis brazos son espectaculares"} correct={["My arms are spectacular"]} />, <TranslateSentenceContainer sentence={"Siempre pelearé por ti"} correct={["I will always fight for you"]} />, <TranslateSentenceContainer sentence={"My name is Rafael Nadal"} correct={["Mi nombre es rafael nadal"]} />]
         }
         
     }
